93648438956e1b4758762e8b36f56af120f2e36a
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2021-10-08 01:45:25 -0300

    remove #

--- scripts/script-final.py
+++ scripts/script-final.py
@@ -11,19 +11,19 @@ from cd_votos import coletar_votos
 # - data inicio
 # - data fim
 # - paginacao maxima
-#definir_data("01/02/2020", "01/07/2020", 7)
+definir_data("01/02/2020", "01/07/2020", 7)
 
 ####### COLETA DAS ATIVIDADES NO PLENARIO
-#coletar_eventos()
+coletar_eventos()
 
 ###### FILTRO POR SESSAO DELIBERATIVA
-#filtrar_deliberativa()
+filtrar_deliberativa()
 
 ###### COLETA DO ID DE CADA VOTACAO
-#pegar_id_votacao()
+pegar_id_votacao()
 
 ###### CRIACAO DE URL DE VOTACAO NOMINAL
-#definir_link_nominal()
+definir_link_nominal()
 
 ###### COLETA DOS DADOS DE VOTACOES
 coletar_votos()
